da7e1c3cb1daf88699e9313c534be782ac686a98
harimohanraj89 <hari.mohanraj89@gmail.com>
2018-10-02 12:02:37 +0530

    Damn, actual protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def self.protected_launch?
+    ENV["BASIC_AUTH_USERNAME"].present? && ENV["BASIC_AUTH_PASSWORD"].present?
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,8 @@
 class RecipesController < ApplicationController
+  if protected_launch?
+    http_basic_authenticate_with name: ENV["BASIC_AUTH_USERNAME"], password: ENV["BASIC_AUTH_PASSWORD"]
+  end
+
   def show
     @recipe = Recipe.find_by(slug: params[:id])
   end
