4e82f4c91427438b980c579725cf840147d8d855
Hari Mohanraj <hari.mohanraj89@gmail.com>
2019-01-12 22:38:41 +0530

    Fetch recipes from /api/recipes (#9)
    This PR fetches recipes from the `/api/recipes` endpoint and populates the `Recipes` component.

--- app/javascript/components/Recipes.js
+++ app/javascript/components/Recipes.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import axios from "axios"
 
 class Recipes extends React.Component {
   constructor () {
@@ -7,20 +8,52 @@ class Recipes extends React.Component {
 
     this.state = {
       loading: true,
+      error: false,
       recipes: []
     }
   }
 
   componentDidMount () {
-    console.log("RECIPES MOUNTED")
+    axios({
+      method:'get',
+      url:'/api/recipes',
+      responseType:'json'
+    })
+      .then((response) => {
+        this.setState({
+          loading: false,
+          recipes: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   render () {
-    return (
-      <React.Fragment>
-        Loading...
-      </React.Fragment>
-    );
+    if (this.state.error) {
+      return (
+        <React.Fragment>
+          Something went wrong, please try again!
+        </React.Fragment>
+      );
+    } else if (this.state.loading) {
+      return (
+        <React.Fragment>
+          Loading...
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <ul>
+          {this.state.recipes.map(recipe => (
+            <li key={recipe.title}>
+              <strong>{recipe.title}</strong>: {recipe.description}
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
