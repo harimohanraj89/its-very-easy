8279f0dc8a4a2153a337e8a0aed3115e1561daa0
harimohanraj89 <hari.mohanraj89@gmail.com>
2019-01-20 20:04:31 +0530

    Change Recipes to stateless component

--- app/javascript/components/Recipes.js
+++ app/javascript/components/Recipes.js
@@ -1,22 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-class Recipes extends React.Component {
-  recipes() {
-    return (this.props.recipes || []);
-  }
-
-  render () {
-    return (
-      <ul>
-        {this.recipes().map(recipe => (
-          <li key={recipe.title}>
-            <strong>{recipe.title}</strong>: {recipe.description}
-          </li>
-        ))}
-      </ul>
-    );
-  }
+const Recipes = ({recipes}) => {
+  return (
+    <ul>
+      {recipes.map(recipe => (
+        <li key={recipe.title}>
+          <strong>{recipe.title}</strong>: {recipe.description}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 Recipes.propTypes = {
