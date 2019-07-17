a880e4bb89f77bb7c88fc24d09e2eb531cf04bd6
Hari Mohanraj <hari.mohanraj89@gmail.com>
2019-07-17 18:53:27 +0530

    [Day 020] Better slug validation (#22)
    [This exception](https://sentry.io/organizations/hari-mohanraj/issues/1114824450/?project=1503544&query=url%3A%22http%3A%2F%2Fwww.itsveryeasy.in%2Fadmin%2Frecipes%2F45%2Fedit%22&statsPeriod=14d) shows that recipes can be created without slugs. This is problematic since slugs are the identifiers that are used for URL construction, etc.
    
    This PR validates the presence of slugs on recipes, and also hides the slug input to prevent it from being manually cleared by users.

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,11 +1,13 @@
 module Admin
   class RecipesController < Fae::BaseController
-
+    def create
+      puts params[:recipe].inspect
+      super
+    end
     private
 
     def build_assets
       @item.build_hero_image if @item.hero_image.blank?
     end
-
   end
 end
