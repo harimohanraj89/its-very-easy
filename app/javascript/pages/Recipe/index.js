8fd6d1711b06de97d7d371efa57e4a80cbd0b896
harimohanraj89 <hari.mohanraj89@gmail.com>
2019-08-07 18:57:53 +0530

    Fix style bug

--- app/javascript/pages/Recipe/index.js
+++ app/javascript/pages/Recipe/index.js
@@ -10,68 +10,66 @@ import CardContent from '@material-ui/core/CardContent';
 
 const Recipe = ({recipe}) => {
   return (
-    <div className={styles.potato}>
-      <div className="recipe">
-        <Grid container spacing={24}>
-          <Grid xs={12} item>
-            <Typography variant="h3" gutterBottom>
-              {recipe.title}
-            </Typography>
+    <div className="recipe">
+      <Grid container spacing={24}>
+        <Grid xs={12} item>
+          <Typography variant="h3" gutterBottom>
+            {recipe.title}
+          </Typography>
 
-            <Typography variant="body1">
-              {recipe.description}
-            </Typography>
+          <Typography variant="body1">
+            {recipe.description}
+          </Typography>
 
-            <hr />
-          </Grid>
-
-          <Grid xs={12} md={6} item>
-            <Card>
-              <CardContent>
-                <Typography variant="h4" gutterBottom>
-                  Ingredients
-                </Typography>
-                <List>
-                  {
-                    recipe.recipe_ingredients.map((ingredient) => {
-                      return (
-                        <ListItem>
-                          <ListItemText>
-                            {ingredient.name}
-                          </ListItemText>
-                        </ListItem>
-                      )
-                    })
-                  }
-                </List>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid xs={12} md={6} item>
-            <Card>
-              <CardContent>
-                <Typography variant="h4" gutterBottom>
-                  Steps
-                </Typography>
-                <List>
-                  {
-                    recipe.recipe_steps.map((step) => {
-                      return (
-                        <ListItem>
-                          <ListItemText>
-                            {step.step}
-                          </ListItemText>
-                        </ListItem>
-                      )
-                    })
-                  }
-                </List>
-              </CardContent>
-            </Card>
-          </Grid>
+          <hr />
         </Grid>
-      </div>
+
+        <Grid xs={12} md={6} item>
+          <Card>
+            <CardContent>
+              <Typography variant="h4" gutterBottom>
+                Ingredients
+              </Typography>
+              <List>
+                {
+                  recipe.recipe_ingredients.map((ingredient) => {
+                    return (
+                      <ListItem>
+                        <ListItemText>
+                          {ingredient.name}
+                        </ListItemText>
+                      </ListItem>
+                    )
+                  })
+                }
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <Grid xs={12} md={6} item>
+          <Card>
+            <CardContent>
+              <Typography variant="h4" gutterBottom>
+                Steps
+              </Typography>
+              <List>
+                {
+                  recipe.recipe_steps.map((step) => {
+                    return (
+                      <ListItem>
+                        <ListItemText>
+                          {step.step}
+                        </ListItemText>
+                      </ListItem>
+                    )
+                  })
+                }
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
   );
 }
